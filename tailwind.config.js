7fc9a7b4382cfc7c064cc961360c76dc8c58b839
Nathan <nathan.marques176@gmail.com>
2023-04-09 01:51:31 -0300

    feat: netlify.toml

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: () => ({
-        hero: "url('./src/images/bg.webp')",
+        hero: "url('/src/images/bg.webp')",
       }),
     },
   },
